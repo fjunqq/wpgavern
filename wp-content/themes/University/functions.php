8dc8b41555bb18d235f1acb5c26362f39cd032b7
fangjun <fjun99@gmail.com>
2014-10-05 20:25:45 +0800

    first test

--- wp-content/themes/University/functions.php
+++ wp-content/themes/University/functions.php
@@ -191,4 +191,6 @@ add_action('admin_enqueue_scripts', 'gavern_enqueue_admin_js_and_css');
 
 // remove the generator metatag due security reasons
 remove_action('wp_head', 'wp_generator');
+
+//test note
 // EOF
